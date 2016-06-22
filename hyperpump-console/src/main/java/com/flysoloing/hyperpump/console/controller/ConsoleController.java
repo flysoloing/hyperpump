e0283a2fac397b29157bb84766e6e6f9f9cd28a6
flysoloing <flysoloing@gmail.com>
2016-06-23 01:47:40 +0800

    use velocity engine process something

--- hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
+++ hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
@@ -1,10 +1,14 @@
 package com.flysoloing.hyperpump.console.controller;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 
 /**
  * @author laitao
@@ -15,9 +19,14 @@ public class ConsoleController {
 
     private static Logger logger = LoggerFactory.getLogger(ConsoleController.class);
 
+    @Autowired
+    private VelocityConfigurer velocityConfigurer;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         logger.info("访问首页");
+        VelocityEngine velocityEngine = velocityConfigurer.getVelocityEngine();
+        Template template = velocityEngine.getTemplate("index.vm");
         return "index";
     }
 }
