7f219de7669e0aa73f048bf65ad759d62d9cac42
flysoloing <flysoloing@gmail.com>
2016-06-23 19:42:55 +0800

    update at 0623

--- hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
+++ hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
@@ -1,10 +1,12 @@
 package com.flysoloing.hyperpump.console.controller;
 
+import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 
 /**
  * @author laitao
@@ -15,9 +17,20 @@ public class ConsoleController {
 
     private static Logger logger = LoggerFactory.getLogger(ConsoleController.class);
 
+    private VelocityConfigurer velocityConfigurer;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         logger.info("访问首页");
+        VelocityEngine velocityEngine = velocityConfigurer.getVelocityEngine();
         return "index";
+    }
+
+    public VelocityConfigurer getVelocityConfigurer() {
+        return velocityConfigurer;
+    }
+
+    public void setVelocityConfigurer(VelocityConfigurer velocityConfigurer) {
+        this.velocityConfigurer = velocityConfigurer;
     }
 }
