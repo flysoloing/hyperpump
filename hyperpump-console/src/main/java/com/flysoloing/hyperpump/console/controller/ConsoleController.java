1a6641401dddff14c0756835fa86e387bb6b5429
flysoloing <flysoloing@gmail.com>
2016-07-20 00:05:48 +0800

    Add flysoloing-common dependency

--- hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
+++ hyperpump-console/src/main/java/com/flysoloing/hyperpump/console/controller/ConsoleController.java
@@ -1,19 +1,18 @@
 package com.flysoloing.hyperpump.console.controller;
 
+import com.flysoloing.common.extention.VelocityViewResolverSupport;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.context.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationObjectSupport;
 import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
 import org.springframework.web.servlet.view.velocity.VelocityLayoutView;
 import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
@@ -32,7 +31,7 @@ import java.util.Map;
  * @since 2016-06-22 17:27:29
  */
 @Controller
-public class ConsoleController extends ViewResolverSupport {
+public class ConsoleController extends VelocityViewResolverSupport {
 
     private static Logger logger = LoggerFactory.getLogger(ConsoleController.class);
 
@@ -100,9 +99,14 @@ public class ConsoleController extends ViewResolverSupport {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("test", "embed velocity engine render3333333333333测试");
         String velocityConfigurerName = "velocityConfigurer";
-        String name = "index.vm";
+        String name = "index";
         Map<String, String> map = new HashMap<String, String>();
-        String result = render(velocityConfigurerName, name, model, request, response);
+        String result = null;
+        try {
+            result = render(velocityConfigurerName, name, model, request, response);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
         map.put("result", result);
         return map;
     }
@@ -113,9 +117,14 @@ public class ConsoleController extends ViewResolverSupport {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("test", "navTest|embed velocity engine render3333333333333测试");
         String velocityConfigurerName = "velocityConfigurer";
-        String name = "taskNodeList.vm";
+        String name = "taskNodeList";
         Map<String, String> map = new HashMap<String, String>();
-        String result = render(velocityConfigurerName, name, model, request, response);
+        String result = null;
+        try {
+            result = render(velocityConfigurerName, name, model, request, response);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
         map.put("result", result);
         return map;
     }
@@ -127,9 +136,14 @@ public class ConsoleController extends ViewResolverSupport {
         model.put("test", "navTest|embed velocity engine render3333333333333测试");
         model.put("namespace", namespace);
         String velocityConfigurerName = "velocityConfigurer";
-        String name = "namespace.vm";
+        String name = "namespace";
         Map<String, String> map = new HashMap<String, String>();
-        String result = render(velocityConfigurerName, name, model, request, response);
+        String result = null;
+        try {
+            result = render(velocityConfigurerName, name, model, request, response);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
         map.put("result", result);
         return map;
     }
